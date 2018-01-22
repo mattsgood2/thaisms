9f783780cd0933bdf8a5615228077e380d33d312
matthew goodman <mattsgood2@aol.com>
2018-01-22 19:13:17 +0000

    REMOVED SECRET_KEYS

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+
 import os
+
 
 def get_env_variable(var_name):
     try:
@@ -22,6 +24,7 @@ def get_env_variable(var_name):
         else:
             raise ImproperlyConfigured(error_msg)
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -30,10 +33,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-3&#p$l-^#mjtw$r3f1*!ikwprdsya(74c+#p08^!fu$a1n2p@'
+SECRET_KEY = get_env_variable('SECRET_KEY')
+
+#
 
 # Twilio-infomation #
-TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- SMS/SMS/wsgi.py
+++ SMS/SMS/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+#dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SMS.settings")
+
 
 application = get_wsgi_application()
