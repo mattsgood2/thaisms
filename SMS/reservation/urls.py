24a59975e97dcbdb834cb629337a562b07d6dad2
matthew goodman <mattsgood2@aol.com>
2018-01-29 13:49:23 +0000

    removed / from urls

--- SMS/reservation/urls.py
+++ SMS/reservation/urls.py
@@ -6,11 +6,11 @@ urlpatterns = [
     url(r'^$', SmsView.as_view(), name=None),
 
     url(r'^$', ReservationListView.as_view(), name='list'),
-    url(r'^/(?P<pk>[0-9]+)$', ReservationDetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)$', ReservationDetailView.as_view(), name='detail'),
 
     url(r'^new/$', ReservationCreateView.as_view(), name='new_reservation'),
-    url(r'^/(?P<pk>[0-9]+)/edit$', ReservationUpdateView.as_view(), name='update_reservation'),
-    url(r'^/(?P<pk>[0-9]+)/delete$', ReservationDeleteView.as_view(), name='delete_reservation'),
+    url(r'^(?P<pk>[0-9]+)/edit$', ReservationUpdateView.as_view(), name='update_reservation'),
+    url(r'^(?P<pk>[0-9]+)/delete$', ReservationDeleteView.as_view(), name='delete_reservation'),
 
 
 ]
