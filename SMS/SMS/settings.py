1248ffbb8cb438336bf30a1e9eddc86ee1a46fd7
matthew goodman <mattsgood2@aol.com>
2018-02-26 19:04:34 +0000

    removed postgresql as was slowing down laptop and not working correctly, back Sqlite3 for now

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -110,12 +110,8 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'thaisms',
-        'USER': 'postgres',
-        'PASSWORD': os.environ['MY_POSTGRES_PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
