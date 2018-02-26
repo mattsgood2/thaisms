69efe93a5144a1c95292b982078cbacbe24f3df2
matthew goodman <mattsgood2@aol.com>
2018-02-26 12:42:53 +0000

    postgresql added for my DB

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -110,8 +110,12 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'thaisms',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['MY_POSTGRES_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
