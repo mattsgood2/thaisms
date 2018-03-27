1052bf4ad57d4c8deb4080b3cc66bcf9b466f2e0
matthew goodman <mattsgood2@aol.com>
2018-03-27 11:01:22 +0100

    adding fields to menu

--- sms/catalog/models.py
+++ sms/catalog/models.py
@@ -4,14 +4,20 @@ from django.db import models
 class Menu(models.Model):
     food_name = models.CharField(max_length=60)
     slug = models.SlugField(max_length=50, unique=True)
-    description = models.TextField()
+    price = models.DecimalField(max_digits=9,decimal_places=2)
+    old_price = models.DecimalField(max_digits=9, decimal_places=2, blank=True, default=0.00)
+    image = models.ImageField()
+    quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
+    bestselling = models.BooleanField(default=False)
     meta_keywords = models.CharField("Meta Keywords", max_length=255)
     meta_description = models.CharField("Meta Keywords", max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    description = models.TextField()
 
     class Meta:
+        db_table = 'menu'
         ordering = 'food_name'
 
 
