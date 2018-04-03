60de6ae42fcdb8a714b2bb9fffcc004a28610758
matthew goodman <mattsgood2@aol.com>
2018-04-03 13:10:51 +0100

    started the menu html

--- sms/catalog/urls.py
+++ sms/catalog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from catalog.views import MenuList
 
 from . import views
 
 urlpatterns = [
-    url('', views.index, name='index'),
+    #url('', views.index, name='index'),
+    url(r'^$', MenuList.as_view()),
 ]
--- sms/catalog/views.py
+++ sms/catalog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from catalog.models import Menu
 # Create your views here.
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the Menu index.")
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the Menu index.")
+
+class MenuList(ListView):
+    model = Menu
