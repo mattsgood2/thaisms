34b47bda178a0123cca3fbc808e802ec27d50dd9
matthew goodman <mattsgood2@aol.com>
2018-05-11 11:58:38 +0100

    models for cart app

--- sms/cart/models.py
+++ sms/cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from sms.catalog.models import Menu
 # Create your models here.
+
+
+class CartItem(models.Model):
+    cart_id = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField(default=1)
+    menu_item = models.ForeignKey('catalog.Menu', unique=False)
