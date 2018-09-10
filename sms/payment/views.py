d6c1b353ba027442065678320c2d9e1843db9a34
matthew goodman <mattsgood2@aol.com>
2018-09-10 16:11:10 +0100

    working out Paypal

--- sms/payment/views.py
+++ sms/payment/views.py
@@ -21,18 +21,20 @@ def payment_canceled(request):
 def payment_process(request):
     cart = request.session.get(settings.CART_SESSION_ID)
     carts = Cart(request)
-    for price in carts:
 
+    for price in carts:
         # print(price['quantity'])
         # print('your total quantity is {} and menu is {}, total is {}'.format(price['quantity'],price['menu'],price['total_price']))
         print('Your Order is {},'.format(price['menu']))
         # print(price)
+        # def theloop(self):
 
 
     paypal_dict = {
         "business": settings.PAYPAL_RECEIVER_EMAIL,
         "amount": '%.2f' % carts.get_total_price(),
         "item_name": "{}" .format(price), #menu_id = str(menu.id)
+        # "item_name": "{}" .format(price['menu']),
         "invoice": "unique-invoice-id",
         "currency_code": 'GBP',
         "notify_url": request.build_absolute_uri(reverse('paypal-ipn')),
