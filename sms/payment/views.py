b5c6bf34343c26d6eab2d49e2b44ad210fbc4143
Matthew Goodman <mattsgood2@aol.com>
2018-10-05 18:32:05 +0100

    removed old lines of code used for testing

--- sms/payment/views.py
+++ sms/payment/views.py
@@ -22,33 +22,15 @@ def payment_process(request):
     # cart = request.session.get(settings.CART_SESSION_ID)
     carts = Cart(request)
 
-
-    # for items in cart :
-    #
-    #     print (items)
-
-    # for item in cart:
-    #     print(item)
-
     for price in carts:
-    #     # Cart.objects.all():
-    #     # print(price)
-    #     # print(price['menu'])
-    #     # print('your total quantity is {} and menu is {}, total is {}'.format(price['quantity'],price['menu'],price['total_price']))
-    #     # print('Your Order is {},'.format(cart))
-    #     print(cart)
-        # print('{}'.format(price))
-        print(price['menu'])
-        
 
-        # def theloop(self):
-    # zipped =zip(carts)
-    # list(zipped)
+        print('THANKS, TOTAL ORDER COST £{}'.format(price['total_price']))
+
 
     paypal_dict = {
         "business": settings.PAYPAL_RECEIVER_EMAIL,
         "amount": '%.2f' % carts.get_total_price(),
-        "item_name": "{}" .format(price), #menu_id = str(menu.id)
+        "item_name": "TOTAL ORDER COST £{}" .format(price['total_price']), #menu_id = str(menu.id)
         # "item_name": "{}" .format(price['menu']),
         "invoice": "unique-invoice-id",
         "currency_code": 'GBP',
