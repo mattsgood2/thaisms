77869436e91aef068dd037e7bf4d8d0317e7ca7a
Matthew Goodman <mattsgood2@aol.com>
2018-10-09 11:16:27 +0100

    changing forms to model forms to get address

--- sms/checkout/models.py
+++ sms/checkout/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class YourAddress(models.Model):
+    your_address = models.CharField(max_length=200)
+    post_code = models.CharField(max_length=10)
+    
